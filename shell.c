978047e62a4e7efaeeae11bb2fc556692c3ff093
aaarunan <arunan2212@gmail.com>
2022-11-23 18:28:29 +0100

    Override upcalls from os

--- shell.c
+++ shell.c
@@ -3,19 +3,33 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
+#include <termios.h>
+#include <errno.h>
 
+void handler(int num)
+{
+    signal(SIGINT, handler);
+    signal(SIGTSTP, handler);
+    puts("\nlol");
+}
 int main()
 {
+    // signal(SIGINT, handler);
+    // signal(SIGTSTP, handler);
 
+    puts("Oi! ser ut til at du har blitt fiska!");
+    puts("Her er et sikkelig dårlig skall som kan hjelpe deg med å fiske problemen dine \n");
+    puts("Skulle vært en måte å endre directory på en eller annen måte ellerhva?");
+
+    char input[256];
     while (1)
     {
-        char input[30];
         printf("$ ");
 
         fflush(0);
 
         fgets(input, sizeof(input), stdin);
-
         if (strlen(input) < 2)
             continue;
 
@@ -40,16 +54,26 @@ int main()
 
             split = strtok(input, " ");
             command = split;
+            if (strcmp("bash", command) == 0 || strcmp("zsh", command) == 0)
+            {
+                puts("Bra prøvd, men ikke godt nok, kanskje noe annet kan hjelpe?");
+                continue;
+            } else if (strcmp("tmux", command) == 0) {
+                puts("tmux???? hva er det? Hæææø");
+                continue;
+            }
 
             int i = 0;
             while (split != NULL)
             {
                 argv[i] = split;
-                i++;
                 split = strtok(NULL, " ");
+                i++;
             }
+            argv[i] = NULL;
             char path[] = "/usr/bin/";
             strcat(path, command);
+            printf("%s \n", path);
 
             execve(path, argv, envarg);
             memset(argv, 0, sizeof(argv));
